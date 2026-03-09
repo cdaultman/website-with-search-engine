9b70e0fc01fdcab549a32df3d32b331b4d59de89
Caleb Aultman <cdaultman122@gmail.com>
2026-03-08 21:16:03 -0500

    Added basic HTml with placeholders and started JS coding

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // API Key - https://api.giphy.com/v1/gifs/search?api_key=shQ8FuLaj8ovcQshIjpNIeqmtSANPUz3&q=${keyword}&limit=12
+
+let results = document.getElementById("results");
+
+let search = document.getElementById("searchBar");

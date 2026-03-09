d497dd13eb080560294afe21a2d75c9ef742f2b2
Caleb Aultman <cdaultman122@gmail.com>
2026-03-08 21:32:35 -0500

    Finished JS coding for the searchbar

--- script.js
+++ script.js
@@ -2,4 +2,33 @@
 
 let results = document.getElementById("results");
 
-let search = document.getElementById("searchBar");
+let searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let keyword = document.getElementById("searchInput").value;
+
+    if (keyword === "") {
+        alert("Enter a valid keyword");
+        return;
+    };
+    results.innerHTML = "Loading...";
+
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=shQ8FuLaj8ovcQshIjpNIeqmtSANPUz3&q=${keyword}&limit=12`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            results.innerHTML = ""
+            data.data.forEach(function(gif) {
+                let img = document.createElement("img");
+                img.src = gif.images.fixed_height.url;
+                results.appendChild(img);
+            });
+        })
+        .catch(function(error) {
+            results.innerHTML = "An error has occurred.";
+            console.log("Error:", error);
+        });
+});
+
